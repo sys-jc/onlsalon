84311d1ce8e1b983dcf17e483c26944353d7b7a9
root <root@localhost.localdomain>
2020-09-11 15:48:13 +0900

    1st

--- projects/common/src/lib/common.module.ts
+++ projects/common/src/lib/common.module.ts
@@ -27,7 +27,8 @@ import { CommonComponent } from './common.component';
   providers: [ Apollo, HttpLink ],
   exports: [
     CommonComponent,
-    ZipadComponent
+    ZipadComponent,
+    MaterialModule
   ]
 })
 
