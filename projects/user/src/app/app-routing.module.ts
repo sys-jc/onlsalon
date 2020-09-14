715e0d6e2c382ba2b0fa129425c2e1d61f6d9d9e
root <root@localhost.localdomain>
2020-09-15 08:53:39 +0900

    user1st

--- projects/user/src/app/app-routing.module.ts
+++ projects/user/src/app/app-routing.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { UserComponent } from './user.component';
 
+const routes: Routes = [
+  {path: 'user', component: UserComponent},
+  {path: 'user/:form', component: UserComponent},
+  {path: 'user/:form/:dojo', component: UserComponent},
 
-const routes: Routes = [];
+  {path: '**', redirectTo: 'user', pathMatch: 'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- projects/user/src/app/app.module.ts
+++ projects/user/src/app/app.module.ts
@@ -3,14 +3,25 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UserComponent } from './user.component';
+
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from './../../../common/src/lib/common.module';
+import { MaterialModule } from './../../../common/src/lib/material.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    CommonModule,
+    FormsModule,
+    MaterialModule 
   ],
   providers: [],
   bootstrap: [AppComponent]
