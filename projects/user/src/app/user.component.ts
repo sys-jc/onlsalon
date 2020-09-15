c3d8dffd33ce8b01110060447542a877ff15ac9c
root <root@localhost.localdomain>
2020-09-15 18:53:09 +0900

    user1st

--- projects/user/src/app/user.component.ts
+++ projects/user/src/app/user.component.ts
@@ -1,8 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, ParamMap  } from '@angular/router';
 import { OAuthService } from 'angular-oauth2-oidc';
-import { Apollo } from 'apollo-angular';
-import * as Query from './queries';
+// import { Apollo } from 'apollo-angular';
+// import * as Query from './queries';
 import { User, UserService } from 'common';
 
 @Component({
@@ -18,7 +18,7 @@ export class UserComponent implements OnInit {
   public form:string;
   constructor(private route: ActivatedRoute,
               private oauthService: OAuthService,
-              private apollo: Apollo,
+              // private apollo: Apollo,
               public usrsrv: UserService) { }
 
   ngOnInit(): void {
@@ -51,26 +51,26 @@ export class UserComponent implements OnInit {
   .then(() => this.oauthService.tryLogin());
   this.claims = this.oauthService.getIdentityClaims();
   console.log("init",this.claims);
-  this.apollo.watchQuery<any>({
-    query: Query.GetQuery1,
-    variables: { 
-        gid:this.claims.sub,
-        did:this.djid
-      },
-    })
-    .valueChanges
-    .subscribe(({ data }) => {
-      if (data.tbluser.length==0){
-        this.flgEx=false;
+  // this.apollo.watchQuery<any>({
+  //   query: Query.GetQuery1,
+  //   variables: { 
+  //       gid:this.claims.sub,
+  //       did:this.djid
+  //     },
+  //   })
+  //   .valueChanges
+  //   .subscribe(({ data }) => {
+  //     if (data.tbluser.length==0){
+  //       this.flgEx=false;
         this.usrsrv.user.googleid=this.claims.sub;
         this.usrsrv.user.sei=this.claims.family_name;
         this.usrsrv.user.mei=this.claims.given_name;
         this.usrsrv.user.mail=this.claims.email;
-      } else { 
-        this.flgEx=true;
-        this.usrsrv.user = data.tbluser[0];
-      }
-    });
+  //     } else { 
+  //       this.flgEx=true;
+  //       this.usrsrv.user = data.tbluser[0];
+  //     }
+  //   });
   }
 
   public login():void{
@@ -81,47 +81,17 @@ export class UserComponent implements OnInit {
 
   }
   public get_tbldj(dojoid:string):void {
-    this.usrsrv.user= new User();
-    this.apollo.watchQuery<any>({
-      query: Query.GetQuery2,
-      variables: { 
-          did : dojoid
-        },
-      })
-      .valueChanges
-      .subscribe(({ data }) => {
-        this.dojo = data.tbldojo_by_pk.dojoname;
-        console.log(this.dojo);
-      });
+    // this.usrsrv.user= new User();
+    // this.apollo.watchQuery<any>({
+    //   query: Query.GetQuery2,
+    //   variables: { 
+    //       did : dojoid
+    //     },
+    //   })
+    //   .valueChanges
+    //   .subscribe(({ data }) => {
+    //     this.dojo = data.tbldojo_by_pk.dojoname;
+    //     console.log(this.dojo);
+    //   });
   }
-  
-  // public get_tblusr(loginfo):void {
-  //   this.usrsrv.user=[];
-    
-  //   this.apollo.watchQuery<any>({
-  //     query: Query.GetQuery1,
-  //     variables: { 
-  //         gid : loginfo.sub,
-  //         did : this.djid
-  //       },
-  //     })
-  //     .valueChanges
-  //     .subscribe(({ data }) => {
-  //       if (data.tblmember.length==0){
-  //         this.flgEx=false;
-  //         this.usrsrv.user.sei=loginfo.family_name;
-  //         this.member.mei=loginfo.given_name;
-  //         // console.log('getmemid前', this.djid);
-  //         this.get_memid(this.djid);
-  //         this.member.memeda=1;
-  //         this.member.class="未登録";
-  //         // this.member.birth="未登録";
-  //         this.member.mail=loginfo.email;
-  //       } else { 
-  //         this.flgEx=true; 
-  //         this.membs = data.tblmember;
-  //         this.member = this.membs[0];
-  //       }
-  //     });
-  // }
 }
